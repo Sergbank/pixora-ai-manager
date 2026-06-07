c936283019539a7f76b253fd881e5cbf819d12d3
Sergbank <43313104+Sergbank@users.noreply.github.com>
2026-06-07 22:07:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 import os
 
-print("PIXORA AI Manager started")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-print("BOT TOKEN:", os.getenv("BOT_TOKEN"))
-print("OPENAI KEY:", os.getenv("OPENAI_API_KEY"))
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Вітаю 👋\n\nЯ AI-менеджер компанії PIXORA.\n\nЯк до вас звертатися?"
+    )
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+
+    await update.message.reply_text(
+        f"Ви написали:\n\n{text}"
+    )
+
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+print("PIXORA BOT STARTED")
+
+app.run_polling()
